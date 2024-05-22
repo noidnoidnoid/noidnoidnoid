da772e1fed5d2151e8c0a74595b8522347502784
Leon Stevans Farias de Souza <leonstevansx@gmail.com>
2024-05-22 20:26:31 -0300

    feat: add afantasia dvd bounce effect

--- src/pages/ProjectSelector/projectSelector.jsx
+++ src/pages/ProjectSelector/projectSelector.jsx
@@ -3,22 +3,24 @@ import styles from './projectSelector.module.css'
 import AfantasiaBanner from '../../assets/AfantasiaBanner.png'
 import AfantasiaLogo from '../../assets/Logo Afantasia.png'
 import EmptyBanner from '../../assets/EmptyBanner.png'
-import TBALogo from '../../assets/Logo TBA.png'
+//import TBALogo from '../../assets/Logo TBA.png'
 
 export function ProjectSelector() {
     return (
-        <div className={styles.projectSelectorContainer}>
-            <div className={styles.projectContainer}>
-                <img className={styles.projectImg} src={AfantasiaBanner}/>
-                <img className={styles.projectLogo} src={AfantasiaLogo} />
-            </div>
-            <div className={styles.projectContainer}>
-                <img className={styles.projectImg} src={EmptyBanner} alt="" />
-            </div>
-            <div className={styles.projectContainer}>
-                <img className={styles.projectImg} src={EmptyBanner} alt="" />
-            </div>
+        <main>
+            <div className={styles.projectSelectorContainer}>
+                <div className={styles.projectContainer}>
+                    <img className={styles.projectImg} src={AfantasiaBanner}/>
+                    <img className={styles.projectLogo} src={AfantasiaLogo} />
+                </div>
+                <div className={styles.projectContainer}>
+                    <img className={styles.projectImg} src={EmptyBanner} alt="" />
+                </div>
+                <div className={styles.projectContainer}>
+                    <img className={styles.projectImg} src={EmptyBanner} alt="" />
+                </div>
 
-        </div>
+            </div>
+        </main>
     )
 }
