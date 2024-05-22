13bc9d1bd1f4bae8c0f1caf09fad9e3237ae1eb4
Leon Stevans Farias de Souza <leonstevansx@gmail.com>
2024-05-22 18:02:54 -0300

    feat: add prototype dvd bounce

--- src/pages/ProjectSelector/projectSelector.jsx
+++ src/pages/ProjectSelector/projectSelector.jsx
@@ -1,13 +1,16 @@
 import styles from './projectSelector.module.css'
 
 import AfantasiaBanner from '../../assets/AfantasiaBanner.png'
+import AfantasiaLogo from '../../assets/Logo Afantasia.png'
 import EmptyBanner from '../../assets/EmptyBanner.png'
+import TBALogo from '../../assets/Logo TBA.png'
 
 export function ProjectSelector() {
     return (
         <div className={styles.projectSelectorContainer}>
             <div className={styles.projectContainer}>
-                <img className={styles.projectImg} src={AfantasiaBanner} alt="" />
+                <img className={styles.projectImg} src={AfantasiaBanner}/>
+                <img className={styles.projectLogo} src={AfantasiaLogo} />
             </div>
             <div className={styles.projectContainer}>
                 <img className={styles.projectImg} src={EmptyBanner} alt="" />
