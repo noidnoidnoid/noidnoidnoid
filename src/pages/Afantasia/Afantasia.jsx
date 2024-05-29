5869b0c62189845dfb4895ee8375a99d9e9864ca
Leon Stevans Farias de Souza <leonstevansx@gmail.com>
2024-05-29 16:01:29 -0300

    feat: add aloe assets to afantasia page

--- src/pages/Afantasia/Afantasia.jsx
+++ src/pages/Afantasia/Afantasia.jsx
@@ -1,11 +1,23 @@
 import styles from './afantasia.module.css'
 
+import AloeSpin from '../../assets/afantasia assets/Aloe/AloeSpin.gif'
+import AloeStand from '../../assets/afantasia assets/Aloe/AloeStand.gif'
+import AloeTopologyFront from '../../assets/afantasia assets/Aloe/AloeTopologyFront.png'
+import AloeTopologyBack from '../../assets/afantasia assets/Aloe/AloeTopologyBack.png'
+
+
 export function Afantasia() {
     return (
         <main>
-            <div className={styles.container}>
-                <div>
-                    <h1>Afantasia</h1>
+            <div className={styles.assetsContainer}>
+                <div className={styles.assetContainer}>
+                    <div className={styles.imgContainer}>
+                        <img src={AloeSpin} className={styles.assetImg} />
+                        <img src={AloeStand} className={styles.assetImg} />
+                        <img src={AloeTopologyFront} className={styles.assetImg} />
+                        <img src={AloeTopologyBack} className={styles.assetImg} />
+                    </div>
+                    <p className={styles.assetText}>Low Poly Model of Aloe Character</p>
                 </div>
             </div>
         </main>
