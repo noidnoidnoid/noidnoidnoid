8e1677d5a89db06d27c6dfe6c62ccbe6ca28f696
Leon Stevans Farias de Souza <leonstevansx@gmail.com>
2024-05-30 04:47:19 -0300

    feat: add Inventory Scene Assets

--- src/pages/Afantasia/Afantasia.jsx
+++ src/pages/Afantasia/Afantasia.jsx
@@ -1,40 +1,51 @@
-import styles from './afantasia.module.css'
-import { useState, useEffect, useRef } from 'react'
-import { motion } from 'framer-motion'
+import styles from './afantasia.module.css';
+import { useState, useEffect, useRef } from 'react';
+import { motion } from 'framer-motion';
 
-import AloeSpin from '../../assets/afantasia assets/Aloe/AloeSpin.gif'
-import AloeStand from '../../assets/afantasia assets/Aloe/AloeStand.gif'
-import AloeTopologyFront from '../../assets/afantasia assets/Aloe/AloeTopologyFront.png'
-import AloeTopologyBack from '../../assets/afantasia assets/Aloe/AloeTopologyBack.png'
-const AloeAssets = [AloeSpin, AloeStand, AloeTopologyFront, AloeTopologyBack]
+// Importação das imagens
+import AloeSpin from '../../assets/afantasia assets/Aloe/AloeSpin.gif';
+import AloeStand from '../../assets/afantasia assets/Aloe/AloeStand.gif';
+import AloeTopologyFront from '../../assets/afantasia assets/Aloe/AloeTopologyFront.png';
+import AloeTopologyBack from '../../assets/afantasia assets/Aloe/AloeTopologyBack.png';
+const AloeAssets = [AloeSpin, AloeStand, AloeTopologyFront, AloeTopologyBack];
+
+import InventorySceneInGame from '../../assets/afantasia assets/InventoryScene/InventorySceneRenderInGame.gif';
+import InventorySceneRender from '../../assets/afantasia assets/InventoryScene/InventorySceneRender.png';
+import InventorySceneTopology1 from '../../assets/afantasia assets/InventoryScene/InventoryScene-Topology1.png';
+import InventorySceneTopology2 from '../../assets/afantasia assets/InventoryScene/InventoryScene-Topology2.png';
+const InventorySceneAssets = [InventorySceneInGame, InventorySceneRender, InventorySceneTopology1, InventorySceneTopology2];
+
+const Assets = [AloeAssets, InventorySceneAssets];
+const AssetTexts = ["Low Poly Model of Aloe Character", "Inventory Scene In Game, Render and Topology"]
 
 export function Afantasia() {
-
-    const carousel = useRef()
-    const [width, setWidth] = useState(0)
+    const carousel = useRef();
+    const [width, setWidth] = useState(0);
 
     useEffect(() => {
-        setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth)
-    }, [])
+        setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+    }, []);
 
     return (
         <div className={styles.assetsContainer}>
-            <motion.div ref={carousel} className={styles.assetContainer} whileTap={{ cursor: "grabbing"}}>
-                <motion.div 
-                className={styles.imgContainer} 
-                drag="x" 
-                dragConstraints={{ right: 0, left: -width}} 
-                initial={{ x: 100 }}
-                animate={{ x: 0 }}
-                >
-                    {AloeAssets.map(image => (
-                        <motion.div className={styles.item} key={image}>
-                            <img className={styles.assetImg} src={image} alt="Aloe Character" />
-                        </motion.div>
-                    ))}
+            {Assets.map((assetGroup, index) => (
+                <motion.div ref={carousel} className={styles.assetContainer} whileTap={{ cursor: "grabbing"}} key={`group-${index}`}>
+                    <motion.div 
+                        className={styles.imgContainer} 
+                        drag="x" 
+                        dragConstraints={{ right: width, left: -width}} 
+                        initial={{ x: 100 }}
+                        animate={{ x: 0 }}
+                    >
+                        {assetGroup.map((image, imgIndex) => (
+                            <motion.div className={styles.item} key={`${image}-${imgIndex}`}>
+                                <img className={styles.assetImg} src={image} alt="Asset Image" />
+                            </motion.div>
+                        ))}
+                    </motion.div>
+                    <p className={styles.assetText}>{AssetTexts[index]}</p>
                 </motion.div>
-                <p className={styles.assetText}>Low Poly Model of Aloe Character</p>
-            </motion.div>
+            ))}
         </div>
-    )
+    );
 }
